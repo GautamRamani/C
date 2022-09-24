1140ac2392f6d55f95526545c4dcfb4ed76cfe5d
GautamRamani <101443138+GautamRamani@users.noreply.github.com>
2022-09-24 09:16:14 +0530

    commit 24-09-2022

---
+++ 6loop/1for loop/iq3.c
@@ -0,0 +1,64 @@
+// //dimond star pyramid
+// #include<stdio.h>
+// int main(){
+//     int i,j,k,sp=40;
+//     for(i=1;i<=5;i++){
+//         for(j=1;j<=sp-1;j++)
+//         printf(" ");
+//         for(j=1;j<=i;j++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//         sp--;
+//     }
+//     for(i=5;i>=1;i--){
+//         for(j=1;j<=sp;j++)
+//         printf(" ");
+//         for(j=1;j<=i;j++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//         sp++;
+//     }
+// }
+
+
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k,sp=40,a=1;
+//     for(i=1;i<=5;i++){
+//         for(j=1;j<=sp-1;j++)
+//         printf(" ");
+//         for(j=1;j<=i;j++){
+//             printf("%d ",a++);
+//         }
+//         printf("\n");
+//         sp--;
+//     }
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k,sp=40,a=1;
+//     for(i=1;i<=3;i++){
+//         for(j=1;j<=sp-1;j++)
+//         printf(" ");
+//         for(j=1;j<=i;j++){
+//             printf("%d ",a++);
+//         }
+//         printf("\n");
+//         sp--;
+//     }
+//     a=6;
+//     for(i=3;i>=1;i--){
+//         for(j=1;j<=sp;j++)
+//         printf(" ");
+//         for(j=1;j<=i;j++){
+//             printf("%d ",a--);
+//         }
+//         printf("\n");
+//         sp++;
+//     }
+// }
---
+++ 6loop/1for loop/iq4.c
@@ -0,0 +1,41 @@
+//right pascal star pattern
+// #include<stdio.h>
+// int main(){
+//     int i,j;
+//     for(i=1;i<=5;i++){
+//         for(j=1;j<=i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     for(i=4;i>=1;i--){
+//         for(j=1;j<=i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+
+//left pascal star pattern
+// #include<stdio.h>
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+//         for(j=1;j<=5-i;j++){
+//             printf(" ");
+//         }
+//         for(k=1;k<=i;k++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     for(i=4;i>=1;i--){
+//         for(j=1;j<=5-i;j++){
+//             printf(" ");
+//         }
+//         for(k=1;k<=i;k++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
